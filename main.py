f9c8e50c445d9b286e70c1961c5dd6a7c40577b1
Andrei Lefter <92049917+andlft@users.noreply.github.com>
2024-05-08 21:23:49 +0300

    Add GUI

--- main.py
+++ main.py
@@ -1,26 +1,16 @@
 import argparse
 
-from collections import defaultdict
-from helpers.game import Game
-from itertools import combinations
+
 from helpers.read_strategies import read_strategies
-from helpers.plot_results import plot_results
+from GUI import run_GUI
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--rounds', type=int, default=100)
+parser.add_argument('--rounds', type=int, default=20)
 args = parser.parse_args()
 
 strategies = read_strategies(path="strategies")
 
-game_strats = combinations(strategies, 2)
-
-results = defaultdict(lambda : 0)
-
-for strats in game_strats:
-    game = Game(*strats)
-    game_result = game.run_game(args.rounds)
-
-    for key, value in game_result.items():
-        results[key] += value
-
-plot_results(results=results)
+run_GUI(
+    strats=strategies,
+    rounds=args.rounds
+    )
